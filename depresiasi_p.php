42e143adedb95f03853a641e957125317d7c2440
Apriliana2424 <37281134+Apriliana2424@users.noreply.github.com>
2018-07-02 10:04:14 +0700

    Update depresiasi_p.php

--- depresiasi_p.php
+++ depresiasi_p.php
@@ -5,8 +5,10 @@
 
  $id_mesin		= $_POST['id_mesin'];
  $depresiasi_perunit	= $_POST['depresiasi_perunit'];
+ $total_depresiasi	= $_POST['total_depresiasi'];
 
-$sql = "insert into depresiasi values('$id_depresiasi','$id_mesin','$depresiasi_perunit')";  
+
+$sql = "insert into depresiasi values('$id_depresiasi','$id_mesin','$depresiasi_perunit','$total_depresiasi')";  
 $hasil = $kon->query($sql);
 if (!$hasil) {
     echo "Gagal Simpan, silakan diulangi! <br /> ".mysqli_error($koneksidb);
